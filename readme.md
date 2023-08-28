d955c3b36cff41f7d51a65f941fad9e9690ae90b
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-08-28 23:32:18 +0545

    readme file

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+--Used external html templete 
+--add contact form
+--post the data from the contact to the mail
+ 
+
--- services/mailer.js
+++ services/mailer.js
@@ -1,4 +1,3 @@
-
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -6,27 +5,31 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
- 
-    user: 'asimneupane11@gmail.com',
-    pass: ''
+    user: "asimneupane11@gmail.com",
+    pass: "tbghlfogijstuyve",
+  },
+});
 
+transporter.verify((error, success)=> {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
   }
 });
 
+const sendEmail = async (payload) => {
+  const { from, to, message } = payload;
 
-const sendEmail= async(payload)=> {
-    const {from, to , message}= payload;
-  
   const info = await transporter.sendMail({
-    from: from, 
-    to: to, 
-    subject: "Hello ✔", 
-    text: "Hello world?", 
-    html: message.toString(),
+    from: from,
+    to: to,
+    subject: "Hello ✔",
+    text: "Hello world?",
+    html: message,
   });
 
- return info.messageId;
- 
-}
+  return info.messageId;
+};
 
-module.exports= {sendEmail};
+module.exports = { sendEmail };
